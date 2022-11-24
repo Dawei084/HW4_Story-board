0e4e95ce1ee370b47f9a08374b5800ee82f27a2d
Daweihao84 <dwsamurai84@gmail.com>
2022-11-24 21:16:26 +0800

    Initial Commit

---
+++ HW4_Story board/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW4_Story board
+//
+//  Created by Dawei Hao on 2022/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
